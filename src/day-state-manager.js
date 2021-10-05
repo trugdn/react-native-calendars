f03578276dc937db73c7702db0dee01dba820841
trugdn <43042772+trugdn@users.noreply.github.com>
2021-10-05 11:39:49 +0700

    Add files via upload
    add src

---
+++ src/day-state-manager.js
@@ -0,0 +1,35 @@
+import {isToday, isDateNotInTheRange, sameMonth, sameDate, isSaturday, isSunday} from './dateutils';
+import {parseDate, toMarkingFormat} from './interface';
+import XDate from 'xdate';
+
+function getState(day, current, props) {
+  const {minDate, maxDate, disabledByDefault, context} = props;
+  const _minDate = parseDate(minDate);
+  const _maxDate = parseDate(maxDate);
+  let state = '';
+
+  if (context?.date === toMarkingFormat(day)) {
+    state = 'selected';
+  } else if (isToday(day)) {
+    state = 'today';
+  }
+  if (disabledByDefault) {
+    state = 'disabled';
+  } else if (isDateNotInTheRange(_minDate, _maxDate, day)) {
+    state = 'disabled';
+  } else if (!sameMonth(day, current)) {
+    state = 'disabled';
+  }else if (sameDate(day, XDate())) {
+    state = 'today';
+  } else if (isSaturday(day, XDate())) {
+    state = 'saturday';
+  } else if (isSunday(day, XDate())) {
+    state = 'sunday';
+  }
+
+  return state;
+}
+
+module.exports = {
+  getState
+};
